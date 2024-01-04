5135bdad9094c279127eef07c225ad2dd7aaa85c
Gohyo04 <gohyo7@gmail.com>
2024-01-04 12:10:47 +0900

    0104_delete

--- src/main/java/com/winter/app/HomeController.java
+++ src/main/java/com/winter/app/HomeController.java
@@ -3,7 +3,6 @@ package com.winter.app;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,17 +11,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.winter.app.ioc.Robot;
-
 /**
  * Handles requests for the application home page.
  */
 // annotation : 설명, 기능
 @Controller
 public class HomeController {
-	
-	@Autowired
-	private Robot robot;
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
@@ -40,8 +34,6 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		robot.getLeftArm().act();
-		robot.getRightArm().act();
 		return "index";
 	}
 	
