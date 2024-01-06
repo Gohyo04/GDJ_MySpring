6642b237abc83201c3508adef412837ad6c1d814
Gohyo04 <gohyo7@gmail.com>
2024-01-06 23:47:04 +0900

    0106error

--- src/main/java/com/winter/app/departments/DepartmentController.java
+++ src/main/java/com/winter/app/departments/DepartmentController.java
@@ -1,38 +1,37 @@
-//package com.winter.app.departments;
-//
-//import java.util.List;
-//
-//import javax.servlet.http.HttpServletRequest;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.servlet.ModelAndView;
-//
-////@Controller
-////@RequestMapping(value="/departments/*")
-////public class DepartmentController {
-////	
-////	@Autowired
-////	private DepartmentService departmentService;
-////	
-////	@RequestMapping(value="detail", method = RequestMethod.GET)
-////	public ModelAndView detail(DepartmentDTO departmentDTO, ModelAndView mv) throws Exception{
-////		departmentDTO = departmentService.getDetail(departmentDTO);
-////		
-////		mv.addObject("dto",departmentDTO);
-////		mv.setViewName("departments/detail");
-////		return mv;
-////	}
-////	
-////	@RequestMapping(value="list", method = RequestMethod.GET)
-////	public String list(HttpServletRequest request) throws Exception{
-////		DepartmentDAO departmentDAO = new DepartmentDAO();
-////		List<DepartmentDTO> ar = departmentDAO.getList();
-////		
-////		request.setAttribute("list", ar);
-////		return "departments/list";
-////	}
-//	
-//}
+package com.winter.app.departments;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value="/departments/*")
+public class DepartmentController {
+	
+	@Autowired
+	private DepartmentService departmentService;
+	
+	@RequestMapping(value="detail", method = RequestMethod.GET)
+	public ModelAndView detail(DepartmentDTO departmentDTO, ModelAndView mv) throws Exception{
+		departmentDTO = departmentService.getDetail(departmentDTO);
+		
+		mv.addObject("dto",departmentDTO);
+		mv.setViewName("departments/detail");
+		return mv;
+	}
+	
+	@RequestMapping(value="list", method = RequestMethod.GET)
+	public String list(HttpServletRequest request) throws Exception{
+		DepartmentDAO departmentDAO = new DepartmentDAO();
+		List<DepartmentDTO> ar = departmentDAO.getList();
+		
+		request.setAttribute("list", ar);
+		return "departments/list";
+	}
+}
--- src/main/java/com/winter/app/departments/DepartmentService.java
+++ src/main/java/com/winter/app/departments/DepartmentService.java
@@ -6,26 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class DepartmentService {
 	
-//	@Autowired
-//	private DepartmentDAO departmentDAO;
-//	
-//	public DepartmentDTO getDetail(DepartmentDTO departmentDTO) throws Exception{
-//		return departmentDAO.getDetail(departmentDTO);
-//	}
-//	
-//	public List<DepartmentDTO> getList() throws Exception{
-//		return departmentDAO.getList();
-//	}
-//	
-//	public int add(DepartmentDTO departmentDTO) throws Exception{
-//		return departmentDAO.add(departmentDTO);
-//	}
-//	
-//	public int update(DepartmentDTO departmentDTO) throws Exception{
-//		return departmentDAO.update(departmentDTO);
-//	}
-//	
-//	public int delete(DepartmentDTO departmentDTO) throws Exception{
-//		return departmentDAO.delete(departmentDTO);
-//	}
+	@Autowired
+	private DepartmentDAO departmentDAO;
+	
+	public DepartmentDTO getDetail(DepartmentDTO departmentDTO) throws Exception{
+		return departmentDAO.getDetail(departmentDTO);
+	}
+	
+	public List<DepartmentDTO> getList() throws Exception{
+		return departmentDAO.getList();
+	}
+	
+	public int add(DepartmentDTO departmentDTO) throws Exception{
+		return departmentDAO.add(departmentDTO);
+	}
+	
+	public int update(DepartmentDTO departmentDTO) throws Exception{
+		return departmentDAO.update(departmentDTO);
+	}
+	
+	public int delete(DepartmentDTO departmentDTO) throws Exception{
+		return departmentDAO.delete(departmentDTO);
+	}
 }
